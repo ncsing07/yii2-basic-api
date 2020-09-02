5e216c879b7d06b013a94ff70747c9c31c3b037a
ncsing07 <ncsing@bamako888.com>
2020-09-02 14:10:10 +0800

    Initial commit.

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$config = require __DIR__ . '/../config/web.php';
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+(new yii\web\Application($config))->run();
